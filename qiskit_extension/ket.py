f8b6345af9959cea78c0b17ad952d5043613b472
Slope <60516263+Slope86@users.noreply.github.com>
2023-01-16 13:03:16 +0800

    simplify check_valid

--- qiskit_extension/ket.py
+++ qiskit_extension/ket.py
@@ -8,12 +8,12 @@ from qiskit_extension import config
 
 
 class _ConfigMeta(type):
-    _z0: str = "0"
-    _z1: str = "1"
-    _x0: str = "+"
-    _x1: str = "-"
-    _y0: str = "i"
-    _y1: str = "j"
+    __z0: str = "0"
+    __z1: str = "1"
+    __x0: str = "+"
+    __x1: str = "-"
+    __y0: str = "i"
+    __y1: str = "j"
 
     def __new__(cls, name, bases, namespace):
         config_parser = configparser.ConfigParser()
@@ -24,33 +24,33 @@ class _ConfigMeta(type):
                 raise ValueError("Unknown basis.")
             if len(config_parser["ket"][ket]) != 1:
                 raise ValueError("The ket notation should be a single character.")
-            namespace[f"_{ket}"] = config_parser["ket"][ket]
+            namespace[f"__{ket}"] = config_parser["ket"][ket]
 
         return super().__new__(cls, name, bases, namespace)
 
     @property
     def z0(self) -> str:
-        return self._z0
+        return self.__z0
 
     @property
     def z1(self) -> str:
-        return self._z1
+        return self.__z1
 
     @property
     def x0(self) -> str:
-        return self._x0
+        return self.__x0
 
     @property
     def x1(self) -> str:
-        return self._x1
+        return self.__x1
 
     @property
     def y0(self) -> str:
-        return self._y0
+        return self.__y0
 
     @property
     def y1(self) -> str:
-        return self._y1
+        return self.__y1
 
 
 class Ket(metaclass=_ConfigMeta):
@@ -61,14 +61,11 @@ class Ket(metaclass=_ConfigMeta):
         """
         Check if the input label is valid.
         """
-        ket_list = ["^["]
-        for ket in list(cls.__dict__.values())[3:9]:
-            ket_list.append(ket)
-        ket_list.append("]+$")
-        ket_str = "".join(ket_list)
-        ket_str = ket_str.replace("-", R"\-")
+        ket_regex = "^[" + cls.z0 + cls.z1 + cls.x0 + cls.x1 + cls.y0 + cls.y1 + "]+$"
+        # escape the minus sign in the regex
+        # e.g. if ket notation = |0>, |1>, |+>, |->, |i>, |j>, then ket_str = R"^[01+\-rj]+$"
+        ket_regex = ket_regex.replace("-", R"\-")
 
-        # e.g. if ket notation = |0>, |1>, |+>, |->, |i>, |j>, then the ket_str = R"^[01+\-rj]+$"
-        if re.match(ket_str, label) is None:
+        if re.match(ket_regex, label) is None:
             return False
         return True
